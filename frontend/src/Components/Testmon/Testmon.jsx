588ec8af508c502456150cea2ccded294deb4f30
Nikita Agarwal <115351051+nikita-1310@users.noreply.github.com>
2023-06-08 21:26:21 +0530

    Update Testmon.jsx

--- frontend/src/Components/Testmon/Testmon.jsx
+++ frontend/src/Components/Testmon/Testmon.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Slider from "react-slick"
 import ava01 from "../../assets/images/ava-1.jpg"
 import ava02 from "../../assets/images/ava-2.jpg"
-import ava03 from "../../assets/images/ava-3.jpg"
+import ava03 from "../../assets/images/ava-3.jpg" 
 const Testmon = () => {
     const settings ={
         dots:true,
